a12494caf3cfa28652e870044727258ebb2a0cd2
lstratman <lstratman@gmail.com>
2012-07-27 17:33:02 -0400

    Added some missing files.

---
+++ CheckForUpdatesWindow.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows.Forms;
+using wyDay.Controls;
+
+namespace EasyConnect
+{
+    public partial class CheckForUpdatesWindow : Form
+    {
+        public CheckForUpdatesWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected MainForm ParentTabs
+        {
+            get
+            {
+                return Owner as MainForm;
+            }
+        }
+
+        private void CheckForUpdatesWindow_Shown(object sender, EventArgs e)
+        {
+            _progressBar.Style = ProgressBarStyle.Marquee;
+            
+            ParentTabs.AutomaticUpdater.UpToDate += AutomaticUpdater_UpToDate;
+            ParentTabs.AutomaticUpdater.CheckingFailed += AutomaticUpdater_CheckingFailed;
+            ParentTabs.AutomaticUpdater.UpdateAvailable += AutomaticUpdater_UpdateAvailable;
+
+            ParentTabs.CheckForUpdate();
+        }
+
+        void AutomaticUpdater_UpdateAvailable(object sender, EventArgs e)
+        {
+            _statusLabel.Text = "An update is available.  Click \"OK\" to install.";
+            _progressBar.Style = ProgressBarStyle.Blocks;
+            _progressBar.Value = 100;
+
+            _okButton.Enabled = true;
+        }
+
+        private void AutomaticUpdater_CheckingFailed(object sender, FailArgs e)
+        {
+            _statusLabel.Text = "Error occurred while checking for updates.  Please try again later.";
+            _progressBar.Style = ProgressBarStyle.Blocks;
+            _progressBar.Value = 100;
+
+            _okButton.Enabled = true;
+        }
+
+        void AutomaticUpdater_UpToDate(object sender, SuccessArgs e)
+        {
+            _statusLabel.Text = "Application is up-to-date.";
+            _progressBar.Style = ProgressBarStyle.Blocks;
+            _progressBar.Value = 100;
+
+            _okButton.Enabled = true;
+        }
+
+        private void _cancelButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void CheckForUpdatesWindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ParentTabs.AutomaticUpdater.UpToDate -= AutomaticUpdater_UpToDate;
+            ParentTabs.AutomaticUpdater.CheckingFailed -= AutomaticUpdater_CheckingFailed;
+            ParentTabs.AutomaticUpdater.UpdateAvailable -= AutomaticUpdater_UpdateAvailable;
+        }
+
+        private void _okButton_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+    }
+}
---
+++ Options.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using EasyConnect.Protocols;
+
+namespace EasyConnect
+{
+    [Serializable]
+    public class Options
+    {
+        public bool AutoHideToolbar
+        {
+            get;
+            set;
+        }
+
+        public static Options Load()
+        {
+            if (!File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\EasyConnect\\Options.xml"))
+                return new Options();
+
+            using (XmlReader reader = new XmlTextReader(new FileStream(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\EasyConnect\\Options.xml", FileMode.Open)))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(Options));
+                return (Options)serializer.Deserialize(reader);
+            }
+        }
+
+        public void Save()
+        {
+            XmlSerializer serializer = new XmlSerializer(GetType());
+            
+            using (FileStream fileStream = new FileStream(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\EasyConnect\\Options.xml", FileMode.Create))
+            {
+                serializer.Serialize(fileStream, this);
+            }
+        }
+    }
+}
