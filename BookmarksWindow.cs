75c7de11cb56ee9c8559a1c59ea301641833c782
WildGenie <WildGenie@outlook.com>
2014-05-24 15:54:43 +0300

    Update Easy Tab, Check null exeption, Add invoke, Fix same exeption

--- BookmarksWindow.cs
+++ BookmarksWindow.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 using System.Xml;
 using System.Xml.Serialization;
 using EasyConnect.Protocols;
-using Stratman.Windows.Forms.TitleBarTabs;
+using EasyTabs;
 
 namespace EasyConnect
 {
