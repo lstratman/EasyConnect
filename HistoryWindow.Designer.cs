348b2519c0fe3d976d9b30d7abae0deed0c1acf5
Luke Stratman <lstratman@gmail.com>
2017-08-22 13:32:49 -0400

    Added URL bar to history window
    The history window now displays the URL bar (read only) with the ability
    to open the tools menu.

--- HistoryWindow.Designer.cs
+++ HistoryWindow.Designer.cs
@@ -38,7 +38,32 @@
             this._timeHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this._nameHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this._hostHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.toolbarBackground = new System.Windows.Forms.Panel();
+            this._urlPanelContainer = new System.Windows.Forms.Panel();
+            this._toolsButton = new System.Windows.Forms.PictureBox();
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.urlBorder = new System.Windows.Forms.Panel();
+            this.urlBackground = new System.Windows.Forms.Panel();
+            this._iconPictureBox = new System.Windows.Forms.PictureBox();
+            this._toolsMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this._newTabMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this._newWindowMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator10 = new System.Windows.Forms.ToolStripSeparator();
+            this._historyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator11 = new System.Windows.Forms.ToolStripSeparator();
+            this._optionsMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this._toolsMenuSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this._updatesMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this._toolsMenuSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this._aboutMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this._exitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.historyContextMenu.SuspendLayout();
+            this.toolbarBackground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this._toolsButton)).BeginInit();
+            this.urlBorder.SuspendLayout();
+            this.urlBackground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this._iconPictureBox)).BeginInit();
+            this._toolsMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // historyImageList
@@ -71,17 +96,20 @@
             // 
             // _historyListView
             // 
+            this._historyListView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._historyListView.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
             this._historyListView.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this._historyListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this._timeHeader,
             this._nameHeader,
             this._hostHeader});
-            this._historyListView.Dock = System.Windows.Forms.DockStyle.Fill;
             this._historyListView.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this._historyListView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
-            this._historyListView.Location = new System.Drawing.Point(0, 0);
+            this._historyListView.Location = new System.Drawing.Point(0, 36);
             this._historyListView.Name = "_historyListView";
-            this._historyListView.Size = new System.Drawing.Size(760, 480);
+            this._historyListView.Size = new System.Drawing.Size(760, 444);
             this._historyListView.SmallImageList = this.historyImageList;
             this._historyListView.TabIndex = 1;
             this._historyListView.UseCompatibleStateImageBehavior = false;
@@ -101,17 +129,206 @@
             // 
             this._hostHeader.Text = "Host";
             // 
+            // toolbarBackground
+            // 
+            this.toolbarBackground.BackgroundImage = global::EasyConnect.Properties.Resources.ToolbarBackground;
+            this.toolbarBackground.Controls.Add(this._urlPanelContainer);
+            this.toolbarBackground.Controls.Add(this._toolsButton);
+            this.toolbarBackground.Controls.Add(this.urlTextBox);
+            this.toolbarBackground.Controls.Add(this.urlBorder);
+            this.toolbarBackground.Dock = System.Windows.Forms.DockStyle.Top;
+            this.toolbarBackground.Location = new System.Drawing.Point(0, 0);
+            this.toolbarBackground.Name = "toolbarBackground";
+            this.toolbarBackground.Size = new System.Drawing.Size(760, 36);
+            this.toolbarBackground.TabIndex = 7;
+            // 
+            // _urlPanelContainer
+            // 
+            this._urlPanelContainer.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._urlPanelContainer.BackColor = System.Drawing.Color.White;
+            this._urlPanelContainer.Location = new System.Drawing.Point(34, 8);
+            this._urlPanelContainer.Name = "_urlPanelContainer";
+            this._urlPanelContainer.Size = new System.Drawing.Size(682, 19);
+            this._urlPanelContainer.TabIndex = 0;
+            // 
+            // _toolsButton
+            // 
+            this._toolsButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this._toolsButton.BackColor = System.Drawing.Color.Transparent;
+            this._toolsButton.Image = global::EasyConnect.Properties.Resources.ToolsActive;
+            this._toolsButton.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this._toolsButton.Location = new System.Drawing.Point(727, 5);
+            this._toolsButton.Margin = new System.Windows.Forms.Padding(4, 4, 3, 3);
+            this._toolsButton.Name = "_toolsButton";
+            this._toolsButton.Size = new System.Drawing.Size(27, 27);
+            this._toolsButton.TabIndex = 5;
+            this._toolsButton.TabStop = false;
+            this._toolsButton.Click += new System.EventHandler(this._toolsButton_Click);
+            this._toolsButton.MouseEnter += new System.EventHandler(this._toolsButton_MouseEnter);
+            this._toolsButton.MouseLeave += new System.EventHandler(this._toolsButton_MouseLeave);
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.urlTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.urlTextBox.Font = new System.Drawing.Font("Tahoma", 11.25F);
+            this.urlTextBox.Location = new System.Drawing.Point(34, 8);
+            this.urlTextBox.Margin = new System.Windows.Forms.Padding(9);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(647, 19);
+            this.urlTextBox.TabIndex = 0;
+            this.urlTextBox.WordWrap = false;
+            // 
+            // urlBorder
+            // 
+            this.urlBorder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.urlBorder.BackColor = System.Drawing.Color.Silver;
+            this.urlBorder.Controls.Add(this.urlBackground);
+            this.urlBorder.ForeColor = System.Drawing.Color.Silver;
+            this.urlBorder.Location = new System.Drawing.Point(5, 5);
+            this.urlBorder.Name = "urlBorder";
+            this.urlBorder.Size = new System.Drawing.Size(715, 26);
+            this.urlBorder.TabIndex = 1;
+            // 
+            // urlBackground
+            // 
+            this.urlBackground.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.urlBackground.BackColor = System.Drawing.Color.White;
+            this.urlBackground.Controls.Add(this._iconPictureBox);
+            this.urlBackground.ForeColor = System.Drawing.Color.Silver;
+            this.urlBackground.Location = new System.Drawing.Point(1, 1);
+            this.urlBackground.Name = "urlBackground";
+            this.urlBackground.Size = new System.Drawing.Size(713, 24);
+            this.urlBackground.TabIndex = 2;
+            // 
+            // _iconPictureBox
+            // 
+            this._iconPictureBox.InitialImage = null;
+            this._iconPictureBox.Location = new System.Drawing.Point(4, 4);
+            this._iconPictureBox.Name = "_iconPictureBox";
+            this._iconPictureBox.Size = new System.Drawing.Size(16, 16);
+            this._iconPictureBox.TabIndex = 0;
+            this._iconPictureBox.TabStop = false;
+            // 
+            // _toolsMenu
+            // 
+            this._toolsMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this._toolsMenu.DropShadowEnabled = false;
+            this._toolsMenu.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._toolsMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this._newTabMenuItem,
+            this._newWindowMenuItem,
+            this.toolStripSeparator10,
+            this._historyToolStripMenuItem,
+            this.toolStripSeparator11,
+            this._optionsMenuItem,
+            this._toolsMenuSeparator1,
+            this._updatesMenuItem,
+            this._toolsMenuSeparator2,
+            this._aboutMenuItem,
+            this._exitMenuItem});
+            this._toolsMenu.Name = "_toolsMenu";
+            this._toolsMenu.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this._toolsMenu.ShowImageMargin = false;
+            this._toolsMenu.Size = new System.Drawing.Size(173, 182);
+            this._toolsMenu.VisibleChanged += new System.EventHandler(this._toolsMenu_VisibleChanged);
+            // 
+            // _newTabMenuItem
+            // 
+            this._newTabMenuItem.Name = "_newTabMenuItem";
+            this._newTabMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.T)));
+            this._newTabMenuItem.Size = new System.Drawing.Size(172, 22);
+            this._newTabMenuItem.Text = "New tab";
+            this._newTabMenuItem.Click += new System.EventHandler(this._newTabMenuItem_Click);
+            // 
+            // _newWindowMenuItem
+            // 
+            this._newWindowMenuItem.Name = "_newWindowMenuItem";
+            this._newWindowMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this._newWindowMenuItem.Size = new System.Drawing.Size(172, 22);
+            this._newWindowMenuItem.Text = "New window";
+            this._newWindowMenuItem.Click += new System.EventHandler(this._newWindowMenuItem_Click);
+            // 
+            // toolStripSeparator10
+            // 
+            this.toolStripSeparator10.Name = "toolStripSeparator10";
+            this.toolStripSeparator10.Size = new System.Drawing.Size(169, 6);
+            // 
+            // _historyToolStripMenuItem
+            // 
+            this._historyToolStripMenuItem.Name = "_historyToolStripMenuItem";
+            this._historyToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.H)));
+            this._historyToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this._historyToolStripMenuItem.Text = "History";
+            this._historyToolStripMenuItem.Click += new System.EventHandler(this._historyToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator11
+            // 
+            this.toolStripSeparator11.Name = "toolStripSeparator11";
+            this.toolStripSeparator11.Size = new System.Drawing.Size(169, 6);
+            // 
+            // _optionsMenuItem
+            // 
+            this._optionsMenuItem.Name = "_optionsMenuItem";
+            this._optionsMenuItem.Size = new System.Drawing.Size(172, 22);
+            this._optionsMenuItem.Text = "Options";
+            this._optionsMenuItem.Click += new System.EventHandler(this._optionsMenuItem_Click);
+            // 
+            // _toolsMenuSeparator1
+            // 
+            this._toolsMenuSeparator1.Name = "_toolsMenuSeparator1";
+            this._toolsMenuSeparator1.Size = new System.Drawing.Size(169, 6);
+            // 
+            // _updatesMenuItem
+            // 
+            this._updatesMenuItem.Name = "_updatesMenuItem";
+            this._updatesMenuItem.Size = new System.Drawing.Size(172, 22);
+            this._updatesMenuItem.Text = "Check for update";
+            this._updatesMenuItem.Visible = false;
+            this._updatesMenuItem.Click += new System.EventHandler(this._updatesMenuItem_Click);
+            // 
+            // _toolsMenuSeparator2
+            // 
+            this._toolsMenuSeparator2.Name = "_toolsMenuSeparator2";
+            this._toolsMenuSeparator2.Size = new System.Drawing.Size(169, 6);
+            // 
+            // _aboutMenuItem
+            // 
+            this._aboutMenuItem.Name = "_aboutMenuItem";
+            this._aboutMenuItem.Size = new System.Drawing.Size(172, 22);
+            this._aboutMenuItem.Text = "About...";
+            this._aboutMenuItem.Click += new System.EventHandler(this._aboutMenuItem_Click);
+            // 
+            // _exitMenuItem
+            // 
+            this._exitMenuItem.Name = "_exitMenuItem";
+            this._exitMenuItem.Size = new System.Drawing.Size(172, 22);
+            this._exitMenuItem.Text = "Exit";
+            this._exitMenuItem.Click += new System.EventHandler(this._exitMenuItem_Click);
+            // 
             // HistoryWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(760, 480);
+            this.Controls.Add(this.toolbarBackground);
             this.Controls.Add(this._historyListView);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "HistoryWindow";
             this.Text = "History";
             this.historyContextMenu.ResumeLayout(false);
+            this.toolbarBackground.ResumeLayout(false);
+            this.toolbarBackground.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this._toolsButton)).EndInit();
+            this.urlBorder.ResumeLayout(false);
+            this.urlBackground.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this._iconPictureBox)).EndInit();
+            this._toolsMenu.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -126,5 +343,24 @@
         private System.Windows.Forms.ColumnHeader _timeHeader;
         private System.Windows.Forms.ColumnHeader _nameHeader;
         private System.Windows.Forms.ColumnHeader _hostHeader;
+        private System.Windows.Forms.Panel toolbarBackground;
+        private System.Windows.Forms.Panel _urlPanelContainer;
+        private System.Windows.Forms.PictureBox _toolsButton;
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.Panel urlBorder;
+        private System.Windows.Forms.Panel urlBackground;
+        private System.Windows.Forms.PictureBox _iconPictureBox;
+        private System.Windows.Forms.ContextMenuStrip _toolsMenu;
+        private System.Windows.Forms.ToolStripMenuItem _newTabMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem _newWindowMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator10;
+        private System.Windows.Forms.ToolStripMenuItem _historyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator11;
+        private System.Windows.Forms.ToolStripMenuItem _optionsMenuItem;
+        private System.Windows.Forms.ToolStripSeparator _toolsMenuSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem _updatesMenuItem;
+        private System.Windows.Forms.ToolStripSeparator _toolsMenuSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem _aboutMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem _exitMenuItem;
     }
 }
--- HistoryWindow.cs
+++ HistoryWindow.cs
@@ -13,6 +13,8 @@ using System.Xml.Serialization;
 using EasyConnect.Protocols;
 using EasyConnect.Protocols.Rdp;
 using EasyTabs;
+using EasyConnect.Properties;
+using System.Configuration;
 
 namespace EasyConnect
 {
@@ -75,7 +77,30 @@ namespace EasyConnect
 				foreach (HistoricalConnection historyEntry in historicalConnections)
 					AddToHistory(historyEntry);
 			}
-		}
+
+            _toolsMenu.Renderer = new EasyConnectToolStripRender();
+
+            HtmlPanel urlPanel = new HtmlPanel
+            {
+                AutoScroll = false,
+                Width = _urlPanelContainer.Width,
+                Height = _urlPanelContainer.Height,
+                Left = 0,
+                Top = 0,
+                Font = urlTextBox.Font,
+                Anchor = AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top
+            };
+
+            _urlPanelContainer.Controls.Add(urlPanel);
+
+            _iconPictureBox.Image = new Icon(Icon, 16, 16).ToBitmap();
+            urlPanel.Text = String.Format(
+                    @"<div style=""background-color: #FFFFFF; font-family: Tahoma; font-size: 11.25pt; height: {0}px; color: #9999BF;"">easyconnect://<font color=""black"">history</font></div>",
+                    urlPanel.Height);
+
+            _updatesMenuItem.Visible = ConfigurationManager.AppSettings["checkForUpdates"] != "false";
+            _toolsMenuSeparator2.Visible = ConfigurationManager.AppSettings["checkForUpdates"] != "false";
+        }
 
 		/// <summary>
 		/// List of all connections that the user has made in the last two weeks.
@@ -250,10 +275,134 @@ namespace EasyConnect
 				_applicationForm.Connect(_connections[_historyListView.SelectedItems[0]].Connection);
 		}
 
-		/// <summary>
-		/// Wraps a connection that the user made and the time that they made it.
+        /// <summary>
+		/// Main application instance that this window is associated with, which is used to call back into application functionality.
 		/// </summary>
-		public class HistoricalConnection : ISerializable, IXmlSerializable
+		protected MainForm ParentTabs
+        {
+            get
+            {
+                return (MainForm)Parent;
+            }
+        }
+
+        /// <summary>
+		/// Handler method that's called when the user's cursor goes over <see cref="_toolsButton"/>.  Sets the button's background to the standard
+		/// "hover" image.
+		/// </summary>
+		/// <param name="sender">Object from which this event originated, <see cref="_toolsButton"/> in this case.</param>
+		/// <param name="e">Arguments associated with this event.</param>
+		private void _toolsButton_MouseEnter(object sender, EventArgs e)
+        {
+            _toolsButton.BackgroundImage = Resources.ButtonHoverBackground;
+        }
+
+        /// <summary>
+        /// Handler method that's called when the user's cursor leaves <see cref="_toolsButton"/>.  Sets the button's background to nothing.
+        /// </summary>
+        /// <param name="sender">Object from which this event originated, <see cref="_toolsButton"/> in this case.</param>
+        /// <param name="e">Arguments associated with this event.</param>
+        private void _toolsButton_MouseLeave(object sender, EventArgs e)
+        {
+            if (!_toolsMenu.Visible)
+                _toolsButton.BackgroundImage = null;
+        }
+
+        /// <summary>
+        /// Handler method that's called when the user clicks the "Exit" menu item in the tools menu.  Exits the entire application.
+        /// </summary>
+        /// <param name="sender">Object from which this event originated.</param>
+        /// <param name="e">Arguments associated with this event.</param>
+        private void _exitMenuItem_Click(object sender, EventArgs e)
+        {
+            ((Form)Parent).Close();
+        }
+
+        /// <summary>
+        /// Handler method that's called when the user clicks the "Tools" icon in the toolbar.  Opens up <see cref="_toolsMenu"/>.
+        /// </summary>
+        /// <param name="sender">Object from which this event originated.</param>
+        /// <param name="e">Arguments associated with this event.</param>
+        private void _toolsButton_Click(object sender, EventArgs e)
+        {
+            _toolsButton.BackgroundImage = Resources.ButtonPressedBackground;
+            _toolsMenu.DefaultDropDownDirection = ToolStripDropDownDirection.Left;
+            _toolsMenu.Show(_toolsButton, -1 * _toolsMenu.Width + _toolsButton.Width, _toolsButton.Height);
+        }
+
+        private void _aboutMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox aboutBox = new AboutBox();
+            aboutBox.ShowDialog(ParentTabs);
+        }
+
+        /// <summary>
+		/// Handler method that's called when the user clicks on the "Check for updates" menu item under the tools menu.  Starts the update check process by
+		/// calling <see cref="MainForm.CheckForUpdate"/> on <see cref="ParentTabs"/>.
+		/// </summary>
+		/// <param name="sender">Object from which this event originated.</param>
+		/// <param name="e">Arguments associated with this event.</param>
+		private void _updatesMenuItem_Click(object sender, EventArgs e)
+        {
+            ParentTabs.CheckForUpdate();
+        }
+
+        /// <summary>
+		/// Handler method that's called when the user clicks the "History" menu item under the tools menu.  Creates the history tab if one doesn't exist 
+		/// already and then switches to it.
+		/// </summary>
+		/// <param name="sender">Object from which this event originated.</param>
+		/// <param name="e">Arguments associated with this event.</param>
+		private void _historyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ParentTabs.OpenHistory();
+        }
+
+        /// <summary>
+		/// Handler method that's called when the user clicks the <see cref="_newWindowMenuItem"/> in the tools menu.  Creates a new <see cref="MainForm"/>
+		/// instance and opens it.
+		/// </summary>
+		/// <param name="sender">Object from which this event originated, <see cref="_newWindowMenuItem"/> in this case.</param>
+		/// <param name="e">Arguments associated with this event.</param>
+		private void _newWindowMenuItem_Click(object sender, EventArgs e)
+        {
+            MainForm newWindow = new MainForm(new List<Guid>());
+            ParentTabs.ApplicationContext.OpenWindow(newWindow);
+
+            newWindow.Show();
+        }
+
+        /// <summary>
+		/// Handler method that's called when the user clicks the "New tab" menu item under the tools menu.  Creates a new tab and then switches to it.
+		/// </summary>
+		/// <param name="sender">Object from which this event originated.</param>
+		/// <param name="e">Arguments associated with this event.</param>
+		private void _newTabMenuItem_Click(object sender, EventArgs e)
+        {
+            ParentTabs.AddNewTab();
+        }
+
+        /// <summary>
+		/// Handler method that's called when the user clicks the "Options" menu item under the tools menu.  Creates the options tab if one doesn't exist 
+		/// already and then switches to it.
+		/// </summary>
+		/// <param name="sender">Object from which this event originated.</param>
+		/// <param name="e">Arguments associated with this event.</param>
+		private void _optionsMenuItem_Click(object sender, EventArgs e)
+        {
+            ParentTabs.OpenOptions();
+        }
+
+        private void _toolsMenu_VisibleChanged(object sender, EventArgs e)
+        {
+            if (!_toolsMenu.Visible)
+                _toolsButton.BackgroundImage = null;
+        }
+
+        /// <summary>
+        /// Wraps a connection that the user made and the time that they made it.
+        /// </summary>
+        public class HistoricalConnection : ISerializable, IXmlSerializable
 		{
 			/// <summary>
 			/// Default constructor.
